rkrasiuk/rbuilder

commit 437d63fefec708917b221abac0d1cfabef0f2049

Notify orderpool cleanup without IPC (#327)
## 📝 Summary

Closes #318. This PR uses the block building flow to notify the order
pool to clean old transactions instead of using the IPC path of the EL
node.

## 💡 Motivation and Context

<!--- (Optional) Why is this change required? What problem does it
solve? Remove this section if not applicable. -->

---

## ✅ I have completed the following steps:

* [ ] Run `make lint`
* [ ] Run `make test`
* [ ] Added tests (if applicable)

---------

Co-authored-by: Daniel Xifra <mandinga@gmail.com>

crates/rbuilder/src/live_builder/order_input/mod.rs
@@ -1,6 +1,5 @@
 //! order_input handles receiving new orders from the ipc mempool subscription and json rpc server
 //!
-pub mod clean_orderpool;
 pub mod order_replacement_manager;
 pub mod order_sink;
 pub mod orderpool;
@@ -13,13 +12,16 @@ use self::{
     replaceable_order_sink::ReplaceableOrderSink,
 };
 use crate::primitives::{serialize::CancelShareBundle, BundleReplacementKey, Order};
+use crate::telemetry::{set_current_block, set_ordepool_count};
+use alloy_consensus::Header;
 use jsonrpsee::RpcModule;
 use parking_lot::Mutex;
 use reth_provider::StateProviderFactory;
+use std::time::Instant;
 use std::{net::Ipv4Addr, path::PathBuf, sync::Arc, time::Duration};
 use tokio::{sync::mpsc, task::JoinHandle};
 use tokio_util::sync::CancellationToken;
-use tracing::{info, trace, warn};
+use tracing::{debug, error, info, trace, warn};
 
 use super::base_config::BaseConfig;
 
@@ -182,6 +184,7 @@ pub async fn start_orderpool_jobs<P>(
     global_cancel: CancellationToken,
     order_sender: mpsc::Sender<ReplaceableOrderPoolCommand>,
     order_receiver: mpsc::Receiver<ReplaceableOrderPoolCommand>,
+    header_receiver: mpsc::Receiver<Header>,
 ) -> eyre::Result<(JoinHandle<()>, OrderPoolSubscriber)>
 where
     P: StateProviderFactory + 'static,
@@ -198,8 +201,8 @@ where
         orderpool: orderpool.clone(),
     };
 
-    let clean_job = clean_orderpool::spawn_clean_orderpool_job(
-        config.clone(),
+    let clean_job = spawn_clean_orderpool_job(
+        header_receiver,
         provider_factory,
         orderpool.clone(),
         global_cancel.clone(),
@@ -224,7 +227,7 @@ where
 
         // @Maybe we should add sleep here because each new order will trigger locking
         let mut new_commands = Vec::new();
-        let mut order_receiver = order_receiver;
+        let mut order_receiver: mpsc::Receiver<ReplaceableOrderPoolCommand> = order_receiver;
 
         loop {
             tokio::select! {
@@ -294,4 +297,55 @@ pub fn expand_path(path: PathBuf) -> eyre::Result<PathBuf> {
         .ok_or_else(|| eyre::eyre!("Invalid UTF-8 in path"))?;
 
     Ok(PathBuf::from(shellexpand::full(path_str)?.into_owned()))
+}
+
+/// Performs maintenance operations on every new header by calling OrderPool::head_updated.
+/// Also calls some functions to generate metrics.
+async fn spawn_clean_orderpool_job<P>(
+    header_receiver: mpsc::Receiver<Header>,
+    provider_factory: P,
+    orderpool: Arc<Mutex<OrderPool>>,
+    global_cancellation: CancellationToken,
+) -> eyre::Result<JoinHandle<()>>
+where
+    P: StateProviderFactory + 'static,
+{
+    let mut header_receiver: mpsc::Receiver<Header> = header_receiver;
+
+    let handle = tokio::spawn(async move {
+        info!("Clean orderpool job: started");
+
+        while let Some(header) = header_receiver.recv().await {
+            let block_number = header.number;
+            set_current_block(block_number);
+            let state = match provider_factory.latest() {
+                Ok(state) => state,
+                Err(err) => {
+                    error!("Failed to get latest state: {}", err);
+                    // @Metric error count
+                    continue;
+                }
+            };
+
+            let mut orderpool = orderpool.lock();
+            let start = Instant::now();
+
+            orderpool.head_updated(block_number, &state);
+
+            let update_time = start.elapsed();
+            let (tx_count, bundle_count) = orderpool.content_count();
+            set_ordepool_count(tx_count, bundle_count);
+            debug!(
+                block_number,
+                tx_count,
+                bundle_count,
+                update_time_ms = update_time.as_millis(),
+                "Cleaned orderpool",
+            );
+        }
+
+        global_cancellation.cancel();
+        info!("Clean orderpool job: finished");
+    });
+    Ok(handle)
 }

crates/rbuilder/src/live_builder/mod.rs
@@ -79,6 +79,10 @@ pub trait SlotSource {
     fn recv_slot_channel(self) -> mpsc::UnboundedReceiver<MevBoostSlotData>;
 }
 
+/// Max headers sent to the cleaning task before the main loop blocks.
+/// Cleaning task is super fast so it should never lag behind block building, even 1 should be enough, 10 is super safe.
+const CLEAN_TASKS_CHANNEL_SIZE: usize = 10;
+
 /// Main builder struct.
 /// Connects to the CL, get the new slots and builds blocks for each slot.
 /// # Usage
@@ -151,6 +155,8 @@ where
         let mut inner_jobs_handles = Vec::new();
         let mut payload_events_channel = self.blocks_source.recv_slot_channel();
 
+        let (header_sender, header_receiver) = mpsc::channel(CLEAN_TASKS_CHANNEL_SIZE);
+
         let orderpool_subscriber = {
             let (handle, sub) = start_orderpool_jobs(
                 self.order_input_config,
@@ -159,6 +165,7 @@ where
                 self.global_cancellation.clone(),
                 self.orderpool_sender,
                 self.orderpool_receiver,
+                header_receiver,
             )
             .await?;
             inner_jobs_handles.push(handle);
@@ -247,6 +254,11 @@ where
                 parent_hash = ?payload.parent_block_hash(),
                 "Got header for slot"
             );
+
+            // notify the order pool that there is a new header
+            if let Err(err) = header_sender.send(parent_header.clone()).await {
+                warn!("Failed to send header to builder pool: {:?}", err);
+            }
 
             inc_active_slots();
 

crates/rbuilder/src/live_builder/order_input/clean_orderpool.rs
@@ -1,77 +1,0 @@
-use super::OrderInputConfig;
-use crate::{
-    live_builder::order_input::orderpool::OrderPool,
-    telemetry::{set_current_block, set_ordepool_count},
-};
-use alloy_provider::{IpcConnect, Provider, ProviderBuilder};
-use futures::StreamExt;
-use parking_lot::Mutex;
-use reth_provider::StateProviderFactory;
-use std::{pin::pin, sync::Arc, time::Instant};
-use tokio::task::JoinHandle;
-use tokio_util::sync::CancellationToken;
-use tracing::{debug, error, info};
-
-/// Performs maintenance operations on every new header by calling OrderPool::head_updated.
-/// Also calls some functions to generate metrics.
-pub async fn spawn_clean_orderpool_job<P>(
-    config: OrderInputConfig,
-    provider_factory: P,
-    orderpool: Arc<Mutex<OrderPool>>,
-    global_cancellation: CancellationToken,
-) -> eyre::Result<JoinHandle<()>>
-where
-    P: StateProviderFactory + 'static,
-{
-    let ipc = IpcConnect::new(config.ipc_path);
-    let provider = ProviderBuilder::new().on_ipc(ipc).await?;
-
-    let handle = tokio::spawn(async move {
-        info!("Clean orderpool job: started");
-
-        let new_block_stream = match provider.subscribe_blocks().await {
-            Ok(subscription) => subscription
-                .into_stream()
-                .take_until(global_cancellation.cancelled()),
-            Err(err) => {
-                error!("Failed to subscribe to a new block stream: {:?}", err);
-                global_cancellation.cancel();
-                return;
-            }
-        };
-        let mut new_block_stream = pin!(new_block_stream);
-
-        while let Some(header) = new_block_stream.next().await {
-            let block_number = header.number;
-            set_current_block(block_number);
-            let state = match provider_factory.latest() {
-                Ok(state) => state,
-                Err(err) => {
-                    error!("Failed to get latest state: {}", err);
-                    // @Metric error count
-                    continue;
-                }
-            };
-
-            let mut orderpool = orderpool.lock();
-            let start = Instant::now();
-
-            orderpool.head_updated(block_number, &state);
-
-            let update_time = start.elapsed();
-            let (tx_count, bundle_count) = orderpool.content_count();
-            set_ordepool_count(tx_count, bundle_count);
-            debug!(
-                block_number,
-                tx_count,
-                bundle_count,
-                update_time_ms = update_time.as_millis(),
-                "Cleaned orderpool",
-            );
-        }
-
-        global_cancellation.cancel();
-        info!("Clean orderpool job: finished");
-    });
-    Ok(handle)
-}

commit 51284e32157ae6d9be53e749ef89c6905aac1855

Stop spawn_clean_orderpool_job on shutdown (#349)
## 📝 Summary

Bug introduced in #327. Rbuilder would not stop with `Ctrl-C` because it
would be waiting for `spawn_clean_orderpool_job` to finish. But,
`spawn_clean_orderpool_job` would not handle the cancellation token.

## 💡 Motivation and Context

<!--- (Optional) Why is this change required? What problem does it
solve? Remove this section if not applicable. -->

---

## ✅ I have completed the following steps:

* [x] Run `make lint`
* [x] Run `make test`
* [x] Added tests (if applicable)

---------

Co-authored-by: Daniel Xifra <mandinga@gmail.com>
Co-authored-by: liamaharon <liam.aharon@hotmail.com>

crates/rbuilder/src/live_builder/order_input/mod.rs
@@ -328,33 +328,49 @@ where
     let handle = tokio::spawn(async move {
         info!("Clean orderpool job: started");
 
-        while let Some(header) = header_receiver.recv().await {
-            let block_number = header.number;
-            set_current_block(block_number);
-            let state = match provider_factory.latest() {
-                Ok(state) => state,
-                Err(err) => {
-                    error!("Failed to get latest state: {}", err);
-                    // @Metric error count
-                    continue;
+        loop {
+            tokio::select! {
+                header = header_receiver.recv() => {
+                    if let Some(header) = header {
+                        let block_number = header.number;
+                        set_current_block(block_number);
+                        let state = match provider_factory.latest() {
+                            Ok(state) => state,
+                            Err(err) => {
+                                error!("Failed to get latest state: {}", err);
+                                // @Metric error count
+                                continue;
+                            }
+                        };
+
+                        let mut orderpool = orderpool.lock();
+                        let start = Instant::now();
+
+                        orderpool.head_updated(block_number, &state);
+
+                        let update_time = start.elapsed();
+                        let (tx_count, bundle_count) = orderpool.content_count();
+                        set_ordepool_count(tx_count, bundle_count);
+                        debug!(
+                            block_number,
+                            tx_count,
+                            bundle_count,
+                            update_time_ms = update_time.as_millis(),
+                            "Cleaned orderpool",
+                        );
+                    } else {
+                        info!("Clean orderpool job: channel ended");
+                        if !global_cancellation.is_cancelled(){
+                            error!("Clean orderpool job: channel ended with no cancellation");
+                        }
+                        break;
+                    }
+                },
+                _ = global_cancellation.cancelled() => {
+                    info!("Clean orderpool job: received cancellation signal");
+                    break;
                 }
-            };
-
-            let mut orderpool = orderpool.lock();
-            let start = Instant::now();
-
-            orderpool.head_updated(block_number, &state);
-
-            let update_time = start.elapsed();
-            let (tx_count, bundle_count) = orderpool.content_count();
-            set_ordepool_count(tx_count, bundle_count);
-            debug!(
-                block_number,
-                tx_count,
-                bundle_count,
-                update_time_ms = update_time.as_millis(),
-                "Cleaned orderpool",
-            );
+            }
         }
 
         global_cancellation.cancel();
fdeb4d6aa9b8c29e348fc2146afedeeceac7af6f
Roman Krasiuk <rokrassyuk@gmail.com>
2025-02-03 14:22:42 +0100

    fix launch cmd

--- crates/reth-rbuilder/src/main.rs
+++ crates/reth-rbuilder/src/main.rs
@@ -12,12 +12,6 @@ use rbuilder::{
     telemetry,
 };
 use reth::{chainspec::EthereumChainSpecParser, cli::Cli, primitives::Header};
-use reth_node_builder::{
-    engine_tree_config::{
-        TreeConfig, DEFAULT_MEMORY_BLOCK_BUFFER_TARGET, DEFAULT_PERSISTENCE_THRESHOLD,
-    },
-    EngineNodeLauncher,
-};
 use reth_node_ethereum::{node::EthereumAddOns, EthereumNode};
 use reth_provider::{
     providers::BlockchainProvider, BlockReader, DatabaseProviderFactory, HeaderProvider,
@@ -42,14 +36,6 @@ pub struct ExtraArgs {
     /// Enable the legacy engine on reth binary
     #[arg(long = "engine.legacy", default_value = "false")]
     pub legacy: bool,
-
-    /// Configure persistence threshold for engine experimental.
-    #[arg(long = "engine.persistence-threshold", conflicts_with = "legacy", default_value_t = DEFAULT_PERSISTENCE_THRESHOLD)]
-    pub persistence_threshold: u64,
-
-    /// Configure the target number of blocks to keep in memory.
-    #[arg(long = "engine.memory-block-buffer-target", conflicts_with = "legacy", default_value_t = DEFAULT_MEMORY_BLOCK_BUFFER_TARGET)]
-    pub memory_block_buffer_target: u64,
 }
 
 fn main() {
@@ -62,9 +48,6 @@ fn main() {
 
     if let Err(err) =
         Cli::<EthereumChainSpecParser, ExtraArgs>::parse().run(|builder, extra_args| async move {
-            let engine_tree_config = TreeConfig::default()
-                .with_persistence_threshold(extra_args.persistence_threshold)
-                .with_memory_block_buffer_target(extra_args.memory_block_buffer_target);
             let handle = builder
                 .with_types_and_provider::<EthereumNode, BlockchainProvider<_>>()
                 .with_components(EthereumNode::components())
@@ -77,14 +60,7 @@ fn main() {
                     );
                     Ok(())
                 })
-                .launch_with_fn(|builder| {
-                    let launcher = EngineNodeLauncher::new(
-                        builder.task_executor().clone(),
-                        builder.config().datadir(),
-                        engine_tree_config,
-                    );
-                    builder.launch_with(launcher)
-                })
+                .launch()
                 .await?;
             handle.node_exit_future.await
         })
